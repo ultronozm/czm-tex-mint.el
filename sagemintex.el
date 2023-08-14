08722feb9107f3edc3b102ad632f7a7bb0685c5f
Paul Nelson <ultrono@gmail.com>
2023-08-14 17:49:03 +0200

    cosmetic improvements

--- sagemintex.el
+++ sagemintex.el
@@ -1,11 +1,11 @@
-;;; sagemintex.el --- Execute minted sage blocks in tex buffers -*- lexical-binding: t; -*-
+;;; sagemintex.el --- Executable minted sage blocks in tex buffers -*- lexical-binding: t; -*-
 
 ;; Copyright (C) 2023  Paul D. Nelson
 
 ;; Author: Paul D. Nelson <nelson.paul.david@gmail.com>
 ;; Version: 0.1
 ;; URL: https://github.com/ultronozm/sagemintex.el
-;; Package-Requires: ((emacs "26.1"))
+;; Package-Requires: ((emacs "26.1") (mmm-mode "0.5.9") (sage-shell-mode "0.3"))
 ;; Keywords: tex, tools, convenience
 
 ;; This program is free software; you can redistribute it and/or modify
@@ -33,42 +33,83 @@
 (require 'mmm-region)
 (require 'sage-shell-mode)
 (require 'preview)
-(setq mmm-global-mode 'maybe)
+(require 'tex-fold)
 
-;; Define latex-minted-sage class
-(mmm-add-classes
- '((latex-minted-sage
-    :submode sage-shell:sage-mode
-    :face mmm-default-submode-face
-    :front "\\\\begin{minted}{sage}"
-    :back "\\\\end{minted}"
-    :save-matches 1
-    :insert ((?s sagecode nil @ "\\begin{minted}{sage}" @ "\n" _ "\n" @ "\\end{minted}" @))
-    :submode-hook (lambda () (sagemintex-mode 1)))))
 
-;; Add the class to LaTeX-mode
-(mmm-add-mode-ext-class 'latex-mode "\\.tex\\'" 'latex-minted-sage)
+(defun sagemintex-initialize ()
+  "Initialize `sagemintex-mode'.
+Define the `latex-minted-sage' class and add it to `latex-mode'."
+  (mmm-add-classes
+   '((latex-minted-sage
+      :submode sage-shell:sage-mode
+      :face mmm-default-submode-face
+      :front "\\\\begin{minted}{sage}"
+      :back "\\\\end{minted}"
+      :save-matches 1
+      :insert ((?s sagecode nil @ "\\begin{minted}{sage}" @ "\n" _ "\n" @ "\\end{minted}" @))
+      :submode-hook (lambda () (sagemintex-mode 1)))))
+  (mmm-add-mode-ext-class 'latex-mode "\\.tex\\'" 'latex-minted-sage))
 
-;; Optional: Customize mmm-default-submode-face
-(custom-set-faces
- '(mmm-default-submode-face ((t (:background "#ddffff")))))
+(defvar sagemintex-mode-map (make-sparse-keymap)
+  "Keymap for `sagemintex-mode'.")
+
+(define-minor-mode sagemintex-mode
+  "Minor mode for minted blocks with sage submode."
+  :init-value nil
+  :lighter " MS"
+  :keymap sagemintex-mode-map)
+
+(defun sagemintex--indent-line-narrowed ()
+  "An indent function which works on some modes where `mmm-indent-line' doesn't.
+Works like `mmm-indent-line', but narrows the buffer before indenting to
+appease modes which rely on constructs like (point-min) to indent."
+  (interactive)
+  (save-excursion
+    (back-to-indentation)
+    (mmm-update-submode-region)
+    (let ((indent-function (get
+                            (if (and mmm-current-overlay
+                                     (> (overlay-end mmm-current-overlay) (point)))
+                                mmm-current-submode
+                              mmm-primary-mode)
+                            'mmm-indent-line-function)))
+      (if mmm-current-overlay
+          (save-restriction
+            (narrow-to-region (overlay-start mmm-current-overlay)
+                              (overlay-end mmm-current-overlay))
+	    (save-excursion
+	      ;; no idea why this works, but it does
+	      ;; (goto-char (point-min))
+	      ;; (newline 1)
+	      ;; (backward-delete-char 1)
+	      )
+            (funcall indent-function))
+        (funcall indent-function)))))
+
+(defun sagemintex-enable ()
+  "Enable `sagemintex-mode' in the current buffer."
+  (setq-local indent-line-function #'sagemintex--indent-line-narrowed)
+  (sagemintex-mode 1))
+
+(defun sagemintex-disable ()
+  "Disable `sagemintex-mode' in the current buffer."
+  (sagemintex-mode 0))
 
 (defgroup sagemintex nil
   "Executable minted sage blocks in tex buffers."
   :group 'tex
   :prefix "sagemintex-")
 
-;; customization variable that determines whether to automatically use
-;; tex-fold on new result regions:
 (defcustom sagemintex-auto-fold-results t
   "If non-nil, apply `TeX-fold-region' to new result blocks."
   :type 'boolean
   :group 'sagemintex)
 
 (defmacro sagemintex-with-auto-fold (&rest body)
-  "Conditionally fold the region created by BODY expressions.
+  "Conditionally fold the region created by BODY.
 If `sagemintex-auto-fold-results' is non-nil, then fold the
-region created by the expressions in BODY."
+region between the initial position of point and the final
+position after evaluating BODY."
   `(let ((pos (point)))
      ,@body
      (when sagemintex-auto-fold-results
@@ -145,58 +186,10 @@ lines and enclose with equation* blocks."
 	  (if beg
 	      (preview-region beg end)))))))
 
-
-
 (defun sagemintex-evaluate-latex ()
   "Evaluate current block.  Wrap result in equation* blocks."
   (interactive)
   (sagemintex-evaluate :latex t))
-
-(defvar sagemintex-mode-map (make-sparse-keymap)
-  "Keymap for `sagemintex-mode'.")
-
-(define-minor-mode sagemintex-mode
-  "Minor mode for minted blocks with sage submode."
-  :init-value nil
-  :lighter " MS"
-  :keymap sagemintex-mode-map)
-
-
-(defun sagemintex-enable ()
-  "Enable `sagemintex-mode' in the current buffer."
-  (setq-local indent-line-function #'sagemintex--indent-line-narrowed)
-  (sagemintex-mode 1))
-
-(defun sagemintex-disable ()
-  "Disable `sagemintex-mode' in the current buffer."
-  (sagemintex-mode 0))
-
-(defun sagemintex--indent-line-narrowed ()
-  "An indent function which works on some modes where `mmm-indent-line' doesn't.
-Works like `mmm-indent-line', but narrows the buffer before indenting to
-appease modes which rely on constructs like (point-min) to indent."
-  (interactive)
-  (save-excursion
-    (back-to-indentation)
-    (mmm-update-submode-region)
-    (let ((indent-function (get
-                            (if (and mmm-current-overlay
-                                     (> (overlay-end mmm-current-overlay) (point)))
-                                mmm-current-submode
-                              mmm-primary-mode)
-                            'mmm-indent-line-function)))
-      (if mmm-current-overlay
-          (save-restriction
-            (narrow-to-region (overlay-start mmm-current-overlay)
-                              (overlay-end mmm-current-overlay))
-	    (save-excursion
-	      ;; no idea why this works, but it does
-	      ;; (goto-char (point-min))
-	      ;; (newline 1)
-	      ;; (backward-delete-char 1)
-	      )
-            (funcall indent-function))
-        (funcall indent-function)))))
 
 
 (provide 'sagemintex)
